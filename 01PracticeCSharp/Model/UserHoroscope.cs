8f94d53837b4be0faa440c16afd45ab9189e36d4
kachynskai <kachynskaiv2@gmail.com>
2025-03-26 22:09:34 +0200

    done

--- 01PracticeCSharp/Model/UserHoroscope.cs
+++ 01PracticeCSharp/Model/UserHoroscope.cs
@@ -8,15 +8,8 @@ namespace KMA.ProgrammingInCSharp.Practice1.Model
 {
     class UserHoroscope
     {
-        private DateTime _birthDay;
-        private int _age;
-        private string _westernSign = string.Empty;
-        private string _chineseSign = string.Empty;
-
-
-        public int Age { get => _age; set => _age = value; }
-        public string WesternSign { get => _westernSign; set => _westernSign = value; }
-        public string ChineseSign { get => _chineseSign; set => _chineseSign = value; }
-        public DateTime BirthDay { get => _birthDay; set => _birthDay = value; }
+        private DateTime? _birthDay;
+      
+        public DateTime? BirthDay { get => _birthDay; set => _birthDay = value; }
     }
 }
--- 01PracticeCSharp/ViewModel/HoroscopeViewModel.cs
+++ 01PracticeCSharp/ViewModel/HoroscopeViewModel.cs
@@ -2,74 +2,123 @@
 using KMA.ProgrammingInCSharp.Practice1.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace KMA.ProgrammingInCSharp.Practice1.ViewModel
 {
-    class HoroscopeViewModel
+    class HoroscopeViewModel: INotifyPropertyChanged
     {
         private readonly UserHoroscope _user = new();
-        private RelayCommand _command;
+        private RelayCommand? _calculateSignsCommand;
 
-        public string WesternSign
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
         {
-            get { return _user.WesternSign; }
-            set { _user.WesternSign = value; }
-        }
-        public string ChineseSign
-        {
-            get { return _user.ChineseSign; }
-            set { _user.ChineseSign = value; }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        public int Age {  get { return _user.Age; } set { _user.Age = value; }}
-
-        public DateTime BirthDate { get { return _user.BirthDay; } set { _user.BirthDay = value; }}
-
-        public string ChineseSignRes => CalculateChineseSign();
-        public string WesternSignRes => CalculateWestermSign();
-
-        private string CalculateWestermSign()
+        public DateTime? BirthDate
         {
+            get { return _user.BirthDay; }
+            set {
+                if (_user.BirthDay != value)
+                {
+                    _user.BirthDay = value;
+                    OnPropertyChanged(nameof(BirthDate));
+                    OnPropertyChanged(nameof(Age));
+                    WesternSign = string.Empty;
+                    ChineseSign = string.Empty;
+                    OnPropertyChanged(nameof(WesternSign));
+                    OnPropertyChanged(nameof(ChineseSign));
+
+                    CalculateSignsCommand.NotifyCanExecuteChanged();
+                }
+            }
+        }
+
+        public int? Age => CalculateAge();
+        public string WesternSign { get; private set; } = string.Empty;
+        public string ChineseSign { get; private set; } = string.Empty;
+
+        private int? CalculateAge()
+        {
+            if(BirthDate == null)
+                return null;
+            DateTime today = DateTime.Now;
+            int age = today.Year - BirthDate.Value.Year;
+            if (BirthDate.Value.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+
+            if (age > 135 || BirthDate > today)
+            {
+                MessageBox.Show("You entered invalid Date!", "Exeption!", MessageBoxButton.OK, MessageBoxImage.Error);
+                BirthDate = null;
+                return null;
+            }
+            if (BirthDate.Value.Day == today.Day && BirthDate.Value.Month == today.Month)
+            {
+                MessageBox.Show("Congrats with ur birthdayyy! It seems like we have a party today!", "Woow, it's a ur day!", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            }
+            
+            return age;
+        }
+
+        private string CalculateWesternSign()
+        {
+            if (BirthDate == null)
+                return string.Empty;
+
             string[] zodiacSigns = {
-        "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo",
-        "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"};
+            "Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini",
+            "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius"
+            };
 
-            int[] zodiacStartDays = { 21, 20, 21, 21, 23, 23, 23, 23, 22, 22, 20, 19 };
 
-            int month = BirthDate.Month;
-            int day = BirthDate.Day;
+            int[] zodiacStartDays = { 20, 19, 21, 20, 21, 21, 23, 23, 23, 23, 22, 22 };
 
-            return day < zodiacStartDays[(month + 9) % 12] ? zodiacSigns[(month + 8) % 12] : zodiacSigns[(month + 9) % 12];
+            int month = BirthDate.Value.Month;
+            int day = BirthDate.Value.Day;
+
+            return (day < zodiacStartDays[month - 1]) ? zodiacSigns[month - 1] : zodiacSigns[month % 12];
         }
 
         private string CalculateChineseSign()
         {
-            int year = BirthDate.Year;
-            string[] chineseZodiacSigns = { "Mouse", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep", "Monkey", "Cock", "Dog", "Pig" };
+            if (BirthDate == null)
+                return string.Empty;
+            int year = BirthDate.Value.Year;
+            string[] chineseZodiacSigns = { "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog", "Pig" };
             return chineseZodiacSigns[(year-4) % 12];
         }
 
 
-        //public RelayCommand Command
-        //{
-        //    get
-        //    {
-        //        return _command ??= new RelayCommand(CalculateSigns, CanCalculateSigns);
-        //    }
-        //}
+        public RelayCommand CalculateSignsCommand
+        {
+            get
+            {
+                return _calculateSignsCommand ??= new RelayCommand(CalculateSigns, CanCalculateSigns);
+            }
+        }
 
         private bool CanCalculateSigns()
         {
-            //return BirthDay != null;
-            return true;
+            return BirthDate != null;
+          
         }
 
         private void CalculateSigns()
         {
-           
+            WesternSign = CalculateWesternSign();
+            ChineseSign = CalculateChineseSign();
+            OnPropertyChanged(nameof(WesternSign));
+            OnPropertyChanged(nameof(ChineseSign));
         }
     }
     
